280a8178a72b9b9f3174b86f03d288c0bd9843ee
Bartosz Rybiński <bartoszrybinski@outlook.com>
2020-11-20 16:21:01 +0100

    W połowie zajęć

--- lab5.sql
+++ lab5.sql
@@ -11,4 +11,11 @@ SELECT * FROM kreatura where nazwa LIKE '%or%' AND udzwig BETWEEN 30 and 70;
 SELECT * FROM zasob WHERE MONTH(dataPozyskania) IN(7, 8);
 SELECT * FROM zasob WHERE rodzaj IS NOT NULL ORDER BY waga ASC;
 SELECT * FROM kreatura ORDER BY dataUr ASC LIMIT 5;
-
+SELECT DISTINCT rodzaj FROM zasob;
+SELECT CONCAT(nazwa, ' - ', rodzaj) FROM kreatura WHERE rodzaj LIKE "wi%";
+SELECT nazwa, waga*ilosc FROM zasob WHERE YEAR(dataPozyskania) BETWEEN 2000 AND 2007;
+SELECT nazwa ,0.7*waga*ilosc as 'Waga netto',0.3*waga*ilosc as 'Waga odpadkow' FROM zasob;
+SELECT * FROM zasob WHERE rodzaj IS NULL;
+SELECT DISTINCT(rodzaj) FROM zasob WHERE nazwa LIKE 'Ba%' OR nazwa LIKE '%os' ORDER BY rodzaj asc;
+SELECT avg(waga) AS srednia_waga_wikingow FROM kreatura WHERE rodzaj = 'wiking';
+SELECT rodzaj, avg(waga) AS srednia_waga FROM kreatura GROUP BY rodzaj;
