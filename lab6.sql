e2ac1b8a2bb7e1241cfb94eaa680d3d2642846e1
Bartosz Rybiński <bartoszrybinski@outlook.com>
2020-11-23 14:26:10 +0100

    Po poniedziałkowym labie

--- lab6.sql
+++ lab6.sql
@@ -1,5 +1,5 @@
 #6
-    zadanie 1
+    zadanie1
 SELECT avg(waga) AS srednia_waga_wikingow FROM kreatura WHERE rodzaj = 'wiking';
 SELECT rodzaj, count(*), avg(waga) FROM kreatura GROUP BY rodzaj;
 SELECT rodzaj, avg(year(now())-year(dataUr)) FROM kreatura GROUP BY rodzaj;
@@ -11,3 +11,22 @@ SELECT rodzaj, count(distinct(nazwa)) as ilosc_zasobow FROM zasob GROUP BY rodza
     zadanie3
 SELECT nazwa, idZasobu, ilosc from kreatura k, ekwipunek e where k.idKreatury=e.idKreatury;
 SELECT k.nazwa, e.idZasobu, e.ilosc, z.nazwa from kreatura k, ekwipunek e, zasob z where k.idKreatury=e.idKreatury and e.idZasobu = z.idZasobu ORDER BY k.nazwa;
+SELECT k.idKreatury, k.nazwa, e.idKreatury from kreatura k left join ekwipunek e on k.idKreatury=e.idKreatury WHERE e.idKreatury is null;
+\\SELECT k.idKreatury, k.nazwa from kreatura k where k.idKreatury not in(select idKreatury from ekwipunek where idKreatury is not null);
+    zadanie4
+SELECT k.nazwa, z.nazwa
+FROM kreatura k, zasob z, ekwipunek e
+WHERE k.idKreatury=e.idKreatury
+AND e.idZasobu=z.idZasobu
+AND k.rodzaj='wiking'
+AND YEAR(k.dataUr) BETWEEN 1670 AND 1679;
+// z natural join
+SELECT k.nazwa, z.nazwa
+FROM zasob z, kreatura k natural join ekwipunek e
+WHERE e.idZasobu=z.idZasobu
+AND k.rodzaj='wiking'
+AND YEAR(k.dataUr) BETWEEN 1670 AND 1679;
+
+SELECT k.nazwa, z.rodzaj, k.dataUr FROM kreatura k INNER JOIN ekwipunek e ON k.idKreatury = e.idKreatury INNER JOIN zasob z ON z.idZasobu = e.idZasobu WHERE z.rodzaj='jedzenie' ORDER BY k.dataUr DESC LIMIT 5;
+
+SELECT k.nazwa, k2.nazwa FROM kreatura k, kreatura k2 where k.idKreatury=k2.idKreatury+5;
